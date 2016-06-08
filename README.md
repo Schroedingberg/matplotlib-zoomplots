0f8d83f2484db6cd66047b1cb0cab9aa7a9445a3
Schroedingberg <aaronrebmann@gmail.com>
2016-06-08 20:07:44 +1000

    Created main source and setup.py

--- README.md
+++
@@ -1,2 +0,0 @@
-# matplotlib-zoomplots
-A library for easy access of the zoom functions from the matplotlib gallery http://matplotlib.org/examples/pylab_examples/axes_zoom_effect.html
---
+++ setup.py
@@ -0,0 +1,11 @@
+import os
+from setuptools import setup
+import setuptools_git as setupgit 
+
+
+setup(
+    name = "matplotlib-zoom",
+    packages = ["zoom"],
+    version="0.0",
+    description="A matplotlib interface for generating plots with zoomed subplots"
+)
